76a2e69caedcb1a36f1e6f7dbbb1a76d8e51dcf0
小林大志 <kobayashihiroshi@kobayashihiroshinoMacBook-Air.local>
2022-11-22 12:00:25 +0900

    APIをフロントエンドへ出力

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+// apis
+import { fetchRestaurants } from '../apis/restaurants';
 
 export const Restaurants = () => {
+  useEffect(() => { // 「useEffect( 関数A, [値B])」 => "「値B」に変更があった場合にのみ「関数A」を実行する"
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, []) // []空配列の場合はコンポーネントを表示した初回のみ関数を実行する
+
   return (
     <Fragment>
       レストラン一覧
